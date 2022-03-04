05677efb0e6e6a6f91c681d5acd73f5786398787
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-03-04 14:16:26 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -8,27 +8,16 @@ class Solution{
 public:
 	void merge(int ar1[], int ar2[], int n, int m) {
 	    // code here
-	    int gap = ceil((float)(n + m) / 2);
-          while (gap > 0) {
-            int i = 0;
-            int j = gap;
-            while (j < (n + m)) {
-              if (j < n && ar1[i] > ar1[j]) {
-                swap(ar1[i], ar1[j]);
-              } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
-                swap(ar1[i], ar2[j - n]);
-              } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
-                swap(ar2[i - n], ar2[j - n]);
-              }
-              j++;
-              i++;
-            }
-            if (gap == 1) {
-              gap = 0;
-            } else {
-              gap = ceil((float) gap / 2);
-            }
-          }
+	    int i=n-1,j=0;
+	    
+	    while(i>=0 && j<m){
+	        if(ar1[i]>ar2[j]){swap(ar1[i],ar2[j]); i--; j++;}
+	        else break;
+	    }
+	    
+	    sort(ar1,ar1+n);
+	    sort(ar2,ar2+m);
+	    return ;
 	}
 };
 
