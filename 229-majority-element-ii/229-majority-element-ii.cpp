08fffd6f297e1d4db84b7258fdd0e8f494a30160
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-03-09 22:27:11 +0530

    Time: 11 ms (89.76%), Space: 15.9 MB (52.90%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,37 @@
+//boyer moore voting algo
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int num1=-1,num2=-1,cnt1=0,cnt2=0,sz=nums.size();
+        
+        for(auto it: nums){
+            if(it==num1) cnt1++;
+            else if(it==num2) cnt2++;
+            
+            else if(cnt1==0){
+                num1=it;
+                cnt1=1;
+            }
+            
+            else if(cnt2==0){
+                num2=it;
+                cnt2=1;
+            }
+            
+            else {cnt1--; cnt2--;}
+        }
+        
+        vector<int> ans;
+        
+        cnt1=cnt2=0;
+        for(auto it: nums){
+            if(it==num1) cnt1++;
+            else if(it==num2) cnt2++;
+        }
+        
+        if(cnt1>sz/3) ans.push_back(num1);
+        if(cnt2>sz/3) ans.push_back(num2);
+        
+        return ans;
+    }
+};
