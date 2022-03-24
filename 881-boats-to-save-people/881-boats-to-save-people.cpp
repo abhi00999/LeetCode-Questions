9635eca477557f56d1d96cfa041075f645b9fe5e
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-03-24 12:53:33 +0530

    Time: 395 ms (5.14%), Space: 70.4 MB (5.14%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int possible( multiset<int> &m, int limit){
+        
+        int tot=0;
+        
+        while(m.size()){
+            int top= *m.begin();
+            m.erase(m.find(top));
+            if(top==limit || m.size()==0) {tot++; continue;}
+            auto it= m.lower_bound(limit-top);
+            
+            if(it!=m.end() && *it==limit-top){
+                m.erase(m.find(limit-top));
+                tot++;
+                continue;
+            }
+            else{
+                if(it!=m.begin())it--;
+                
+                if(*it+top<=limit){m.erase(m.find(*it)); tot++;}
+                else {m.erase(m.find(*it)); tot+=2;}
+                continue;
+            }
+        }
+        
+        return tot;
+    
+    }
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans=INT_MAX;
+        sort(people.begin(),people.end());
+        multiset<int> m;
+        for(auto it: people) m.insert(it);
+        
+        return possible(m,limit);
+    }
+};
