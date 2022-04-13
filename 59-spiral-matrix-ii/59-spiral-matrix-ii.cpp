12e15cd7adbc3eda68085d51371d144488b11bba
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-04-13 16:25:21 +0530

    Time: 4 ms (19.27%), Space: 6.5 MB (85.87%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    bool check(int n, int cnt){
-        if(cnt<=n*n) return false;
-        return true;
-    }
-    
-    
     
     vector<vector<int>> generateMatrix(int n) {
         int cnt=0,tot= n*n;
@@ -19,28 +13,24 @@ public:
                 v[up][i]=val;
                 val++;
             }
-            // if(check(n,val)) break;
             up++;
             
             for(int j=up;j<=down;j++){
                 v[j][right]=val;
                 val++;
             }
-            // if(check(n,val)) break;
             right--;
             
             for(int i=right;i>=left;i--){
                 v[down][i]=val;
                 val++;
             }
-            // if(check(n,val)) break;
             down--;
             
             for(int j=down;j>=up;j--){
                 v[j][left]=val;
                 val++;
-            }
-            // if(check(n,val)) break;           
+            }          
             left++;
             
         }
