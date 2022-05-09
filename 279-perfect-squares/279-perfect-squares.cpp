e4edc03c529d965ce520a2e9b8f2127acc8f3e0e
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-09 06:28:34 +0530

    Time: 404 ms (19.81%), Space: 10.1 MB (23.26%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     int numSquares(int n) {
         vector<int> coins;
+        
         for(int i=1;i*i<=10000;i++) coins.push_back(i*i);
 
 		vector<int>res(n+1, INT_MAX);
-
+        
+        //zero can be made with zero coins
 		res[0] = 0;
-        int nn=coins.size();
-
-		for (int i = 0; i < nn; i++) {
-			for (int j =  coins[i]; j <= n; j++) {
-				if (j>=coins[i] && res[j-coins[i]] != INT_MAX)
-					res[j] = min(res[j], 1+res[j-coins[i]]);
-			}
-		}
-
-		return res[n] != INT_MAX ? res[n] : -1;
+        
+        for(int i=1;i<=n;i++){
+            for(auto it: coins){
+                if(i-it>=0 && res[i-it]!=INT_MAX){
+                    res[i]=min(res[i],1+res[i-it]);
+                }
+            }
+        }
+        
+        return res[n];
+        
     }
 };
