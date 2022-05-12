b8619fe247321f8c31c26a12f7744df0397c5371
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-12 15:05:07 +0530

    Time: 4 ms (91.86%), Space: 9 MB (65.84%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
-        if (i == j-1) {
-            res.push_back(num);
-            return;
-        }
-        for (int k = i; k < j; k++) {
-            if (i != k && num[i] == num[k]) continue;
-            swap(num[i], num[k]);
-            recursion(num, i+1, j, res);
-        }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> perms;
+        permute(nums, 0, perms);
+        return perms;
     }
-    vector<vector<int> > permuteUnique(vector<int> &num) {
-        sort(num.begin(), num.end());
-        vector<vector<int> >res;
-        recursion(num, 0, num.size(), res);
-        return res;
+private:
+    void permute(vector<int> nums, int start, vector<vector<int>>& perms) {
+        int n = nums.size();
+        if (start == n - 1) {
+            perms.push_back(nums);
+        } else {
+            for (int i = start; i < n; i++) {
+                if ((i == start) || (nums[i] != nums[start])) {
+                    swap(nums[i], nums[start]);
+                    permute(nums, start + 1, perms);
+                }
+            }
+        }
     }
 };
