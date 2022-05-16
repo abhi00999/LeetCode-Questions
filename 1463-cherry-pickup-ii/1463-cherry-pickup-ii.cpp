533eb8c4160b6e75e25a0c518f7604a5d17a4982
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-16 05:43:02 +0530

    Time: 140 ms (29.37%), Space: 16.3 MB (6.34%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-    int maxChocoUtil(int i, int j1, int j2, int n, int m, vector < vector < int >> 
-    & grid, vector < vector < vector < int >>> & dp) {
-      if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
-        return -1e9;
-
-      if (i == n - 1) {
-        if (j1 == j2)
-          return grid[i][j1];
-        else
-          return grid[i][j1] + grid[i][j2];
-      }
-
-      if (dp[i][j1][j2] != -1)
-        return dp[i][j1][j2];
-
-      int maxi = INT_MIN;
-      for (int di = -1; di <= 1; di++) {
-        for (int dj = -1; dj <= 1; dj++) {
-          int ans;
-          if (j1 == j2)
-            ans = grid[i][j1] + maxChocoUtil(i + 1, j1 + di, j2 + dj, n, m, grid, dp);
-          else
-            ans = grid[i][j1] + grid[i][j2] + maxChocoUtil(i + 1, j1 + di, j2 + dj, n,
-            m, grid, dp);
-          maxi = max(maxi, ans);
+    int dp[75][75][75];
+    int f(int i, int j1, int j2, int r, int c, vector<vector<int> > &grid, vector < vector < vector < int >>> & dp){
+        //if any of the two cross the borders then that path is invalid 
+        if(j1<0 || j2<0 || j1>c-1 || j2>c-1) return -1e9;
+        
+        // we reach the last row that means we have found one of our possible answers
+        if(i==r-1){
+            //if both reach same cell then it is counted only once else count separately
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
         }
-      }
-      return dp[i][j1][j2] = maxi;
+        
+        //after this explore all paths of robots simultaneously and take maximum of all
+        
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        
+        int maxi=INT_MIN;
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                int value;
+                
+                if(j1==j2) value=grid[i][j1];
+                else value= grid[i][j1]+grid[i][j2];
+                
+                value+=f(i+1,j1+dj1, j2+dj2, r, c, grid, dp); 
+                maxi=max(value,maxi);
+            }
+        }
+        return dp[i][j1][j2]=maxi;
     }
-
-    int maximumChocolates(int n, int m, vector < vector < int >> & grid) {
-
-      vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, vector < int
-      > (m, -1)));
-
-      return maxChocoUtil(0, 0, m - 1, n, m, grid, dp);
-    }
+    
     int cherryPickup(vector<vector<int>>& grid) {
-        int r= grid.size(), c= grid[0].size();
-        // memset(dp,-1,sizeof(dp));
-        return maximumChocolates(r,c,grid);
-        // return 0;
+        int n= grid.size(), m= grid[0].size();
+        vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, vector < int> (m, -1)));
+        return f(0,0,m-1, n, m, grid, dp);
     }
 };
