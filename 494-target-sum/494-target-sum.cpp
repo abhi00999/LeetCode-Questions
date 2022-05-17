d520ccd211846b8fde5ee134d33d9d00ba83e4e3
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-17 17:28:58 +0530

    Time: 414 ms (32.28%), Space: 69.9 MB (8.51%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    map<pair<int,int>,int> m;
+    int f(int ind, int sum, int target, vector<int> &nums, vector<vector<int>> &dp){
+        
+        if(ind==0){
+            if(sum+nums[ind]==target && sum-nums[ind]==target) return 2;
+            else if(sum+nums[ind]==target || sum-nums[ind]==target) return 1;
+            
+            return 0;
+        }
+        
+        if(m.find({ind,sum})!=m.end()) return m[{ind,sum}] ;
+        int pos= f(ind-1, sum+nums[ind], target, nums, dp);
+        int neg= f(ind-1, sum-nums[ind], target, nums, dp);
+        
+        return m[{ind,sum}]=pos+neg;
+        
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size(),sum=0;
+        vector<vector<int>> dp(n, vector<int>(1001,-1));
+        
+        return f(n-1, sum, target, nums, dp);
+    }
+};
