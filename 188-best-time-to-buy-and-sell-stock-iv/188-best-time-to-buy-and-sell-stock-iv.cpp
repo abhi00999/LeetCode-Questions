bdfa17e4608d076aca9b3bd77a00c72c8afb8e98
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-19 23:46:15 +0530

    Time: 18 ms (47.14%), Space: 11.8 MB (55.22%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[1005][2][105];
+    int f(int ind, int buy, int tr, int k, vector<int> &prices, int n){
+        
+        if(ind>=n) return 0;
+        if(tr==k) return 0;
+        
+        if(dp[ind][buy][tr]!=-1) return dp[ind][buy][tr];
+        
+        int op1, op2;
+        //buy=1 means we can buy
+        if(buy){
+            op1= -prices[ind] + f(ind+1, 0, tr, k, prices, n);
+            op2= 0+ f(ind+1, 1, tr, k, prices, n);
+        }
+        
+        else{
+            op1= prices[ind]+ f(ind+1, 1, tr+1, k, prices, n);
+            op2= 0+ f(ind+1, 0, tr, k, prices, n);
+        }
+        
+        return dp[ind][buy][tr]=max(op1, op2);
+    }
+    
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size(), tr=0, buy=1;
+        memset(dp,-1,sizeof(dp));
+        //f(0, buy, tr, k, prices) will return maximum profit that can be achieved if we start from 0th index and 
+        //we have option of buying(buy=1) and at max k transactions(current transaction = 0).
+        return f(0, buy, tr, k, prices, n);
+    }
+};
