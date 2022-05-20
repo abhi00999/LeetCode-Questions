0ae08467a8eb2ff9312b197cda18485a8bd190a6
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-21 01:49:19 +0530

    Added solution - LeetHub

---
+++ Evaluation of Postfix Expression - GFG/evaluation-of-postfix-expression.cpp
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+// C++ program to evaluate value of a postfix expression
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to evaluate a postfix expression.
+    int evaluatePostfix(string S)
+    {
+        stack<int> s ;
+        for(int i=0 ; i<S.length() ; i++)
+        {
+            if(S[i]=='*')
+            {
+                int x=s.top() ;
+                s.pop() ;
+                int y=s.top() ;
+                s.pop() ;
+                s.push(x*y) ;
+            }
+            else if(S[i]=='+')
+            {
+                int x=s.top() ;
+                s.pop() ;
+                int y=s.top() ;
+                s.pop() ;
+                s.push(x+y) ;
+            }
+            else if(S[i]=='/')
+            {
+                int x=s.top() ;
+                s.pop() ;
+                int y=s.top() ;
+                s.pop() ;
+                s.push(y/x) ;
+            }
+            else if(S[i]=='-')
+            {
+                int x=s.top() ;
+                s.pop() ;
+                int y=s.top() ;
+                s.pop() ;
+                s.push(y-x) ;
+            }
+            else s.push(S[i]-'0') ;
+        }
+        
+        return s.top() ;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+    
+    cout<<obj.evaluatePostfix(S)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
