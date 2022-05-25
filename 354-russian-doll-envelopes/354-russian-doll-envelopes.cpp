edb60e76139b5271d26bde0b1bb88bec52946802
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-25 07:09:08 +0530

    Time: 637 ms (21.15%), Space: 77.5 MB (91.99%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -3,8 +3,6 @@
 // If we've already sorted E primarily by width, we should then be able to consider a corresponding array of just the heights and realize that the solution would be defined as the longest increasing subsequence of that.
 
 // The only difficulty would be for consecutive envelopes with the same sorted width. To avoid that, we can simply make sure that our sort function sorts height in descending order so that the first envelope encountered for any given width would be the largest one.
-//width will already be sorted 
-//and for height we make longest increasing subsequence so as to maximise the answer
 
 class Solution {
 public:
@@ -13,7 +11,6 @@ public:
              -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
         vector<int> dp;
         for (auto& env : E) {
-            cout<<env[0]<<" "<<env[1]<<"\n";
             int height = env[1];
             int left = lower_bound(dp.begin(), dp.end(), height) - dp.begin();
             if (left == dp.size()) dp.push_back(height);
