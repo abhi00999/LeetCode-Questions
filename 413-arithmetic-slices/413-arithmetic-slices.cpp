ce506705c87e319435d302675ca45a31f8bf9639
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-05-28 02:05:06 +0530

    Time: 8 ms (11.27%), Space: 7.3 MB (86.98%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& v) {
-        int ans=0,cnt=2,n=v.size();
-        if(n<=2) return 0;
-        int diff= v[1]-v[0];
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int len=1,ans=0, diff=-1e9, n=nums.size();
         
-        for(int i=2;i<n;i++){
-            if(v[i]-v[i-1]==diff) cnt++;
-            
-            else{
-                if(cnt>=3){
-                    int val= cnt-3+1;
-                    ans+= (val*(val+1))/2;
-                }
-                diff= v[i]-v[i-1];
-                cnt=2;
+        for(int i=1;i<n;i++){
+            // cout<<len<<" ";
+            if(diff==-1e9){
+                len++;
+                diff=nums[i]-nums[i-1];
+                continue;
             }
+            
+            int cur= nums[i]-nums[i-1];
+            if(cur==diff){
+                len++;
+            }
+            else{
+                diff=cur;
+                if(len>=3) ans+= (len-1)*len - (len*(len+1))/2+1;
+                len=2;
+            }
+            
         }
-        if(cnt>=3){
-            int val= cnt-3+1;
-            ans+= (val*(val+1))/2;
-        }
+        if(len>=3) ans+= (len-1)*len - (len*(len+1))/2+1;
         return ans;
-        
     }
 };
