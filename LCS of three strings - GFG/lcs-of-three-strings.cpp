7b2cad10ef3b1490018a6ed35d4699191185e48c
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-01 22:45:26 +0530

    Added solution - LeetHub

--- LCS of three strings - GFG/lcs-of-three-strings.cpp
+++ LCS of three strings - GFG/lcs-of-three-strings.cpp
@@ -32,9 +32,25 @@ int f(string &s1, string &s2, string &s3, int i, int j, int k){
     return  dp[i][j][k]= max({op1, op2, op3, op4, op5, op6});
 }
 
-int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
+int LCSof3 (string s1, string s2, string s3, int n1, int n2, int n3)
 {
     // your code here
-    memset(dp,-1, sizeof(dp));
-    return f(A,B,C, n1-1, n2-1, n3-1);
+    memset(dp,0, sizeof(dp));
+    
+    for(int i=0;i<=n1;i++){
+        for(int j=0;j<=n2;j++){
+            for(int k=0;k<=n3;k++){
+                if(i==0 || j==0 || k==0) dp[i][j][k]=0;
+                
+                else{
+                    if(s1[i-1]==s2[j-1] && s2[j-1]==s3[k-1]) dp[i][j][k]=1+dp[i-1][j-1][k-1];
+                    else{
+                        dp[i][j][k]= max({dp[i-1][j][k], dp[i][j][k-1], dp[i][j-1][k], dp[i-1][j-1][k], dp[i-1][j][k-1], 
+                            dp[i][j-1][k-1]});
+                    }
+                }
+            }
+        }
+    }
+    return dp[n1][n2][n3];
 }
