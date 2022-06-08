54b7eff29ea124e366637526fca0b9210cc2b47c
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-09 05:27:11 +0530

    Time: 28 ms (19.86%), Space: 13 MB (95.86%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& ma) {
-      int m=ma.size();
-        int n=ma[0].size();
-        vector<int>row(m,-1);
-        vector<int>col(n,-1);
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(ma[i][j]==0)
-                {
-                    row[i]=1;
-                    col[j]=1;
+    void setZeroes(vector<vector<int>>& matrix) {
+      int col0=1, m=matrix.size(), n=matrix[0].size();
+    
+        for(int i=0;i<m;i++){
+            //checking if 0 is present in the 0th column or not
+            if(matrix[i][0]==0) col0=0;
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-     for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(row[i]!=-1)
-                {
-                    ma[i][j]=0;
-                }
-                if(col[j]!=-1)
-                {
-                    ma[i][j]=0;
-                }
+        //traversing in the reverse direction and
+        //checking if the row or col has 0 or not
+        //and setting values of matrix accordingly.
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
             }
+            if(col0==0) matrix[i][0]=0;
         }
     }
 };
