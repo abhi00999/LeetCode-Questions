b78558de1a7f6273afae887b388d5cb90a7a34c0
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-16 10:38:24 +0530

    Time: 127 ms (8.36%), Space: 14.2 MB (84.85%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    
     int minTaps(int n, vector<int>& nums) {
         
-        int min_reachable=0;
+        // int min_reachable=0;
         int max_reachable=0;
         int open=0;
         
         while(max_reachable<n){
-            
+            int cur_max= max_reachable;
             for(int i=0;i<nums.size();i++){
-                if(i-nums[i]<=min_reachable && i+nums[i]>max_reachable){
-                    max_reachable= i+nums[i];
+                if(i-nums[i]<=max_reachable && i+nums[i]>cur_max){
+                    cur_max= i+nums[i];
                 }
             }
             
-            if(min_reachable== max_reachable) return -1;
+            if(cur_max== max_reachable) return -1;
             open++;
-            min_reachable= max_reachable;
+            max_reachable= cur_max;
         }
         
         return open;
