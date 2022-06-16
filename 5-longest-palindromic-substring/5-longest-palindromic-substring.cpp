54a55ccffd33da787d3d3e366a8fbcf5f9ccf61a
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-16 08:00:34 +0530

    Time: 238 ms (46.85%), Space: 29 MB (38.23%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,63 +1,29 @@
-
-// In brute force solution, we do unnecessary recompution while validating palindomes.
-// For example : if we know string "aba" is palindrome then "cabac" must be palindrome as left and right are equal.
-
-// Solution 2: Using DP
-
-// P(i, j) == P(i+1, j-1) && s[i] == s[j];
-
-// Base cases :
-
-// //One character
-// P(i, i) = true;
-
-// //Two character
-// P(i, i+1) = s[i] == s[i+1];
-
-// Time Complexity - O(N^2), Space Complexity - O(N^2) (caching all substring)
-
-class Solution
-{
+class Solution {
 public:
-    string longestPalindrome(string s)
-    {
-        int n = s.size();
-        if (n == 0)
-            return "";
-
-        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
-        bool dp[n][n];
-
-        //Initialize with false
-
-        memset(dp, 0, sizeof(dp));
-
-        //Every Single character is palindrome
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        string ans = "";
-        ans += s[0];
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (s[i] == s[j])
-                {
-                    //If it is of two character OR if its susbtring is palindrome.
-                    if (j - i == 1 || dp[i + 1][j - 1])
-                    {
-                        //Then it will also a palindrome substring
-                        dp[i][j] = true;
-
-                        //Check for Longest Palindrome substring
-                        if (ans.size() < j - i + 1)
-                            ans = s.substr(i, j - i + 1);
+    string longestPalindrome(string s) {
+        int n=s.size(),maxi=1;
+        int arr[n+1][n+1];
+        memset(arr, 0, sizeof(arr));
+        string res;
+        res.push_back(s[0]);
+        
+        for(int i=0;i<n;i++) arr[i][i]=1;
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]) {arr[i][i+1]=1; res= s.substr(i,2); maxi=2;}
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j] && arr[i+1][j-1]==1){
+                    if(j-i+1>maxi){
+                        maxi=j-i+1;
+                        res=s.substr(i,maxi);
                     }
+                    arr[i][j]=1;
                 }
             }
         }
-        return ans;
+        
+        return res;
     }
 };
