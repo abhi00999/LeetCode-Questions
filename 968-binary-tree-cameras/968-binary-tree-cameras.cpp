2b79ce546d418dbdb7f30baefeed45435e5e7d73
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-17 09:27:50 +0530

    Time: 21 ms (33.50%), Space: 21.2 MB (55.06%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    struct it{
+        TreeNode* root;
+        bool hasCamera;
+        bool monitored;
+    };
+    map< it , int> m;
+    int f(TreeNode* root, bool hasCamera, bool monitored){
+        if(root==NULL) return 0;
+        
+        
+        if(hasCamera) return 1+ f(root->left, false, true) + f(root->right, false, true);
+        
+        if(monitored){
+            int putCam= 1+ f(root->left, false, true) + f(root->right, false, true);
+            int noCam= f(root->left, false, false) + f(root->right, false, false);
+            return min(putCam, noCam);
+        }
+        
+        if (root->val != 0) return root->val;
+        int putCam= 1+ f(root->left, false, true)+ f(root->right, false, true);
+        
+        int leftCam=INT_MAX, rightCam=INT_MAX;
+        if(root->left) leftCam= f(root->left, true, false)+ f(root->right, false, false);
+        if(root->right) rightCam= f(root->left, false, false )+ f(root->right, true, false);
+        
+        return root->val= min({putCam, leftCam, rightCam});             
+    }
+    int minCameraCover(TreeNode* root) {
+        
+        
+        return f(root,false, false);
+    }
+};
