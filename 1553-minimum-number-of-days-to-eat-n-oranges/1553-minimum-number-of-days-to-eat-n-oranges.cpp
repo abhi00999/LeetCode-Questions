9f073cb7de1ccd43ce4b0a6a6eb0bdef53b812f0
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-17 14:24:53 +0530

    Time: 15 ms (68.63%), Space: 18.8 MB (35.38%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    map<int,int> dp;
+    int f(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        if(dp.count(n)) return dp[n];
+        
+        int op1=1e9, op2=1e9, op3=1e9;
+        
+        if(n%2==0) op2= 1+ f(n/2);
+        else if(n-1>=0) op2= 2+ f((n-1)/2);
+        
+        if(n%3==0) op3= 1+ f(n/3);
+        else if(n%3==1 && n-1>=0) op3= 2+ f((n-1)/3);
+        else if(n-2>=0) op3= 3+ f((n-2)/3);
+        
+        
+        return dp[n]= min( op2, op3);
+        
+    }
+    int minDays(int n) {
+        
+        return f(n);
+    }
+};
