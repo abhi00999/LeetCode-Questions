cf9e883ed01db10d7f322751faa26f084851b596
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-20 16:03:29 +0530

    Time: 59 ms (60.98%), Space: 19.1 MB (67.59%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -10,25 +10,6 @@ public:
         
         while(!q.empty()){
             int sz=q.size();
-            
-//             while(sz--){
-//                 pair<int,int> top= q.front();
-//                 q.pop();
-//                 if(top.first==x) return ans;
-                
-//                 int num=top.first, dir= top.second;
-                
-//                 if(s.find(num)!=s.end()) continue;
-//                 s.insert(num);
-                
-//                 if(num<=2000+b){
-//                     q.push({num+a,1});
-//                 }
-//                 if(num-b>=0 && (dir==0 || dir==1)){
-//                     q.push({num-b,2});
-//                 }
-                
-//             }
             while(sz--){
                 auto curr = q.front() ;
                 q.pop() ;
@@ -43,7 +24,7 @@ public:
                 s.insert(num);
                 
                 
-                if((curr.second == 1 || curr.second==0) && num-b>=0) {
+                if((curr.second == 1 || curr.second==0) && num-b>0) {
                     q.push({(num-b),2});
                 }
                 if(num <= 2000+b){
