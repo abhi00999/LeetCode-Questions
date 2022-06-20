d3f02b3721db5950f7f5087091bf3a393340d707
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-20 23:23:29 +0530

    Time: 19 ms (18.08%), Space: 7.6 MB (6.17%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        int n=nums.size();
+        int n=size(nums);
         vector<vector<int> >v(n, vector<int>(2,1));  
         
+        // v[ind][0]--> stores length of max. subsequence ending at index=ind when last diff is positive
+        // v[ind][1]--> stores length of max. subsequence ending at index=ind when last diff is negative
         int res=1;
         
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 int val=nums[i]-nums[j];
+                // if current diff is positive then we add 1 to last index's negative ending and vice-versa
                 if(val>0){
                     v[i][0]= max(v[i][0], v[j][1]+1);
                 }
