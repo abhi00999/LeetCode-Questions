79652a2fa53ac9bfbee8da71a467c7602667fc7d
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-21 13:50:57 +0530

    Time: 5 ms (73.15%), Space: 7.2 MB (42.69%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -12,8 +12,8 @@ public:
         if(i<0 && j>=0) return false;
         
         if(j<0 && i>=0){
-            // in this case text has length 0, so it will match with pattern only of it contains all stars
-            // so we run a for loop
+            // in this case text has length 0, so it will match with pattern only if pattern is as follows
+            // alternating stars --> for eg. a*b*c* 
             while(i>=0){
                 if(pattern[i]=='*') i-=2;
                 else return false;
@@ -27,10 +27,12 @@ public:
             return dp[i][j]=f(i-1, j-1, pattern, text, dp);
         }
         
-        //in the below case we are considering if '*' corresponds to a zero or single character, it will deal with all 
+        //in the below case we are considering if '*' corresponds to a zero or more characters, it will deal with all 
         //cases of all length too
         if(pattern[i]=='*'){            
             char ch = pattern[i-1];
+            // val is initially equal to the case when we take 0 characters
+            // we are doing i-2 bcz if we use pattern[i-1] then we can choose from (i-2)
             bool val= f(i-2, j, pattern, text, dp);
             
             for(int k=j;k>=0;k--){
