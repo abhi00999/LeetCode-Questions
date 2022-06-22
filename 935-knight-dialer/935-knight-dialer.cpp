668f1137ac8d8172f776ec435807ccbeaef9df5d
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-22 11:37:38 +0530

    Time: 164 ms (67.27%), Space: 37.2 MB (35.86%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -11,31 +11,31 @@ public:
         if(last==5) return 0;
         else{
             if(last==0){
-                return dp[last][len]=(f(4, len+1, n,dp)%mod+ f(6, len+1, n, dp)%mod+mod)%mod;
+                return dp[last][len]=f(4, len+1, n,dp)%mod+ f(6, len+1, n, dp)%mod;
             }
             if(last==1){
-                return dp[last][len]=(f(6, len+1, n, dp)%mod+ f(8, len+1, n, dp)%mod+mod)%mod;
+                return dp[last][len]=f(6, len+1, n, dp)%mod+ f(8, len+1, n, dp)%mod;
             }
             if(last==2){
-                return dp[last][len]=(f(7, len+1, n, dp)%mod+ f(9, len+1, n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(7, len+1, n, dp)%mod+ f(9, len+1, n,dp)%mod;
             }
             if(last==3){
-                return dp[last][len]=(f(4, len+1, n, dp)%mod+ f(8, len+1, n, dp)%mod+mod)%mod;
+                return dp[last][len]=f(4, len+1, n, dp)%mod+ f(8, len+1, n, dp)%mod;
             }
             if(last==4){
-                return dp[last][len]=(f(0, len+1, n,dp)%mod+ f(3, len+1, n,dp)%mod+f(9,len+1,n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(0, len+1, n,dp)%mod+ f(3, len+1, n,dp)%mod+f(9,len+1,n,dp)%mod;
             }
             if(last==6){
-                return dp[last][len]=(f(0, len+1, n, dp)%mod+ f(1, len+1, n, dp)%mod+f(7,len+1,n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(0, len+1, n, dp)%mod+ f(1, len+1, n, dp)%mod+f(7,len+1,n,dp)%mod;
             }
             if(last==7){
-                return dp[last][len]=(f(6, len+1, n,dp)%mod+ f(2, len+1, n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(6, len+1, n,dp)%mod+ f(2, len+1, n,dp)%mod;
             }
             if(last==8){
-                return dp[last][len]=(f(1, len+1, n,dp)%mod+ f(3, len+1, n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(1, len+1, n,dp)%mod+ f(3, len+1, n,dp)%mod;
             }
             if(last==9){
-                return dp[last][len]=(f(2, len+1, n,dp)%mod+ f(4, len+1, n,dp)%mod+mod)%mod;
+                return dp[last][len]=f(2, len+1, n,dp)%mod+ f(4, len+1, n,dp)%mod;
             }
         }
         return 0;
@@ -49,7 +49,6 @@ public:
             ans+=(f(i,1,n,dp)+mod)%mod;
         }
         
-        if(ans<0) return (ans+mod)%mod;
         return (ans)%mod;
     }
 };
