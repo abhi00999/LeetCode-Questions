36f48732c015d29551c45f8770cfe2542da9ea86
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-22 12:55:07 +0530

    Time: 6 ms (15.09%), Space: 7.4 MB (40.26%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
-    void f(int last, int cur_ind, vector<string>& wordDict, string s, vector<vector<string> > &res, vector<string> &cur, vector<bool> &dp){
-        if(cur_ind==s.size()+1){
-            res.push_back(cur);
-            return;
-        }
-        
-        for(int i=cur_ind;i<=s.size();i++){
-            if(find(wordDict.begin(),wordDict.end(),s.substr(last, i-last)) != wordDict.end()){
-                
-                cur.push_back(s.substr(last, i-last));
-                f(i,i+1,wordDict, s, res, cur, dp);
-                cur.pop_back();
-            }
-        }
-        
-    }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size()+1,false);
-        dp[0] = true;
-        for(int i=0;i<=s.size();++i){
-            for(int j=0;j<i;++j){
-                if(dp[j])
-                {
-                    if(find(wordDict.begin(),wordDict.end(),s.substr(j,i-j)) != wordDict.end()){
-                        dp[i] = true;
-                        break;
-                    }
-                }
-                
+        //insert all the words in the set
+        unordered_set<string> set;
+        vector<string> res;
+        for(auto word:wordDict)
+            set.insert(word);
+        //to store the current string 
+        string curr="";
+        findHelper(0,s,curr,set,res);
+        return res;
+    }
+    
+    void findHelper(int ind,string s,string curr,unordered_set<string> set,vector<string>& res)
+    {
+        if(ind==s.length())
+        {
+            //we have reached end
+            curr.pop_back(); //remove the trailing space
+            res.push_back(curr);
+        }
+        string str="";
+        for(int i=ind;i<s.length();i++)
+        {
+            //get every substring and check if it exists in set
+            str.push_back(s[i]);
+            if(set.count(str))
+            {
+                //we have got a word in dict 
+                //explore more and get other substrings
+                findHelper(i+1,s,curr+str+" ",set,res);
             }
         }
-        // for(auto it: dp) cout<<it<<" ";
-        // cout<<"\n";
-        vector<vector<string>> res;
-        vector<string> cur,ans;
-        f(0,1,wordDict, s, res, cur, dp);
-        
-        for(auto it: res){
-            string tmp="";
-            for(auto it1: it){
-                tmp+=" ";
-                tmp+=it1;
-            }
-            if(tmp.size())tmp=tmp.substr(1);
-            ans.push_back(tmp);
-        }
-        return ans;
-        
     }
 };
