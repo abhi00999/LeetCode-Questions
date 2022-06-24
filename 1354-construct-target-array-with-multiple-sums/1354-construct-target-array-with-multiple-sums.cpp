a39ef420a0ea4d5688e2bcf8b939f08d4cb08fdf
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-24 10:44:38 +0530

    Time: 74 ms (39.86%), Space: 29.9 MB (61.54%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -12,10 +12,17 @@ public:
             
             pq.pop();
             
+            // suppose we have to find d(as that will be the old element) from this --> {a, b, c, a+b+c+d}
+            // Now sum at this point is = 2*a+2*b+2*c+d
+            // we subtract largest from sum(2*a+2*b+2*c+d -(a+b+c+d)) we get a+b+c
+            // Now it is obvious that a+b+c<a+b+c+d, so if that is not the case we return false
+             
             if(sum==0 || sum>=largest) return false;
             
+            // we get d by doing this--> (a+b+c+d)%(a+b+c) {better than subtracting repeatedly}
             int old= largest%sum;
             
+            // old wil be equal to zero only if sum=1(This is only possible if array has only 2 elements)
             if(sum!=1 && old==0) return false;
             
             pq.push(old);
