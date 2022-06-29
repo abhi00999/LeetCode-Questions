3a516eb9db4204ad0d44c2bdd0f871a66bae2341
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-29 19:28:56 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (89.06%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,3 +1,6 @@
+// Case 1 :mid is equal to the peak element ( Observation : mid element is greater than its neighbors )
+// Case 2: mid lies on the right of our result peak ( Observation : Our peak element search space is leftside )
+// Case 3 : mid lies on the left. ( Observation : Our peak element search space is rightside )
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
