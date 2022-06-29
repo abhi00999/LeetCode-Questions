1cf895ad0a9ef0f2be60106c5072bcbcc64ed356
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-06-29 11:25:41 +0530

    Time: 243 ms (33.87%), Space: 12.1 MB (63.86%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,3 +1,18 @@
+// Intuition :
+
+// Sort the array in decreasing order of their heights and if two people have same height, sort them in increasing order of their [1] index. (We used compare function for this)
+// [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]] => [[7,0], [7,1], [6,1], [5,0], [5,2], [4,4]]
+// Now we constructed an array res which will have elements in order of sorted array at indexes based on the index of their [1] position.
+// Now insert the elements according to the order in res
+// [[7,0]] (insert [7,0] at index 0)
+// [[7,0],[7,1]] (insert [7,1] at index 1)
+// [[7,0],[6,1],[7,1]] (insert [6,1] at index 1)
+// [[5,0],[7,0],[6,1],[7,1]] (insert [5,0] at index 0)
+// [[5,0],[7,0],[5,2],[6,1],[7,1]] (insert [5,2] at index 2)
+// [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] (insert [4,4] at index 4)
+// Time Complexity : O(N * N)
+// Space Complexity : O(N)
+
 class Solution {
 public:
     static bool compare(vector<int> &a,vector<int> &b){
