664dff739d70fc0ace784e9eaca4d0731f2c0355
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-07-08 13:45:05 +0530

    Time: 93 ms (77.66%), Space: 12.3 MB (46.81%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -3,32 +3,32 @@ public:
     
     int dp[105][105][50];
     
-    int f(int idx, vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target,int prev){
+    int f(int idx,int val, vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target,int prev){
         // cout<<idx<<" "<<target<<"\n";
         if(target<0) return 1e7;
         if(idx == m)      // If we have visited all houses
         {
-            if(target == 0)       // If neighbours are same as required, then no more cost is required.
+            if(target == val)       // If neighbours are same as required, then no more cost is required.
                 return 0;
             return 1e7;     // If neighbours are not same, then this build is not valid.
         }
         
-        if(dp[idx][target][prev]!=-1) return dp[idx][target][prev];   
+        if(dp[idx][val][prev]!=-1) return dp[idx][val][prev];   
         
         if(houses[idx] != 0)       // If house is already coloured.
         {
-            return dp[idx][target][prev] = f(idx+1,houses, cost, m ,n, target-(prev!=houses[idx]), houses[idx]);
+            return dp[idx][val][prev] = f(idx+1, val+(prev!=houses[idx]),houses, cost, m ,n, target, houses[idx]);
         }
         int ans = INT_MAX;         // If we are colouring house, then find minimum cost.
         for(int i=1;i<=n;i++)
         {
-            ans = min(ans,cost[idx][i-1]+f(idx+1,houses, cost, m ,n, target-(prev!=i), i));
+            ans = min(ans,cost[idx][i-1]+f(idx+1, val+(prev!=i),houses, cost, m ,n, target, i));
         }
-        return dp[idx][target][prev] = ans;
+        return dp[idx][val][prev] = ans;
     }
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
         memset(dp,-1,sizeof(dp));
-        int ans= f(0, houses, cost, m, n, target,40);
+        int ans= f(0,0, houses, cost, m, n, target,40);
         cout<<ans<<" "; 
         return (ans>=1e7)? -1: ans;
     }
