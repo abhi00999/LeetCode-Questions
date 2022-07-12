f54712f08ba95c4b5a76ca23ae6d7b642bfd0d40
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-07-12 15:24:40 +0530

    Added solution - LeetHub

---
+++ K larger values - GFG/k-larger-values.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	//code
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int arr[n];
+	    
+	    for(int i=0;i<n;i++) cin>>arr[i];
+	    
+	    sort(arr,arr+n);
+	    for(int i=n-1;i>=n-k;i--){
+	        cout<<arr[i]<<" ";
+	    }
+	    cout<<'\n';
+	}
+	return 0;
+}
