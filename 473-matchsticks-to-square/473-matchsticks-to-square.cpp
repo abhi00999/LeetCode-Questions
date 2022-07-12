0e1cbe9941cf868621be73e9e67680d9cb8975ba
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-07-12 12:01:52 +0530

    Time: 250 ms (62.87%), Space: 10.8 MB (27.32%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,31 +1,50 @@
+// DP+ bitmask solution(great sol but tough to understand), see previous backtracking solution too 
 class Solution {
 public:
     
-    int ans;
-    void f(int idx, int a, int b, int c, int d, int val, vector<int> &v){
-        if(idx==v.size()){
-            if(a==b && b==c && c==d && d==val) {ans=1;}
-            return;
+    bool makesquare(vector<int>& nums) {
+        int n = nums.size();
+
+        long sum = accumulate(nums.begin(), nums.end(), 0l);
+        if (sum % 4)
+            return false;
+        long sideLen = sum / 4;
+        // need to solve the problem of partitioning nums into four equal subsets each having
+        // sum equal to sideLen
+        vector<int> usedMasks;
+        // validHalfSubsets[i] == true iff the subset represented by bitmask i
+        // has sum == 2*sideLen, AND the subset represented by i can be further partitioned into
+        // two equal subsets. See below for how it is used.
+        vector<bool> validHalfSubsets(1<<n, false);
+
+        // E.g., if n = 5, (1 << 5 - 1) = 11111 represents the whole set
+        int all = (1 << n) - 1;
+        // go through all possible subsets each represented by a bitmask
+        for (int mask = 0; mask <= all; mask++) {
+            long subsetSum = 0;
+            // calculate the sum of this subset
+            for (int i = 0; i < 32; i++) {
+            if ((mask >> i) & 1)
+            subsetSum += nums[i];
+            }
+            // if this subset has what we want
+            if (subsetSum == sideLen) {
+                for (int usedMask : usedMasks) {
+                // if this mask and usedMask are mutually exclusive
+                    if ((usedMask & mask) == 0) {
+                    // then they form a valid half subset whose sum is 2 * sideLen,
+                            // that can be further partitioned into two equal subsets (usedMask and mask)
+                    int validHalf = usedMask | mask;
+                    validHalfSubsets[validHalf] = true;
+                    // if in the past we concluded that the other half is also a valid
+                    // half subset, DONE!
+                    if (validHalfSubsets[all ^ validHalf])
+                        return true;
+                    }
+                }
+                usedMasks.push_back(mask);
+            }
         }
-        
-        if(ans) return;
-        if(a>val || b> val || c> val || d> val) return;
-        f(idx+1, a+v[idx], b, c, d, val, v);
-        f(idx+1, a, b+v[idx], c, d, val, v);
-        f(idx+1, a, b, c+v[idx] , d, val, v);
-        f(idx+1, a, b, c, d+v[idx], val, v);
-        
-    }
-    bool makesquare(vector<int>& v) {
-        sort(v.begin(), v.end(), greater<int>());
-        int sum=0;
-        for(auto it: v) sum+=it;
-        
-        if(sum%4) return false;
-        
-        ans=0;
-        f(0, 0, 0, 0, 0, sum/4, v);
-        
-        return (ans==1);
+        return false;
     }
 };
