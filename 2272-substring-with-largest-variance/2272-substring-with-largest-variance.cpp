a9ea78d4dec4bdd87cc58169a57c0e5d64e91b37
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-07-30 15:12:49 +0530

    Time: 533 ms (16.70%), Space: 7.2 MB (40.66%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    int f(string &s, char x, char y){
+        int n=s.size(), pos=0,neg=0, maxi=0;
+        int arr[n];
+        int g=0;
+        for(int i=0;i<n;i++){
+            if(s[i]==x) arr[i]=1;
+            else if(s[i]==y) arr[i]=-1 , g++;
+            else arr[i]=0;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]==1) pos++;
+            else if(arr[i]==-1) neg++ , g--;
+            
+            if(pos>=neg && pos && neg) maxi= max(maxi, pos-neg);
+            if(pos<neg&&g>0) {pos=0; neg=0;}
+        }
+        
+        return maxi;
+        
+    }
+    int largestVariance(string s) {
+        
+        int ans=0;
+        map<char, int> m;
+        for(auto it: s) m[it]=1;
+        
+        for(char x='a'; x<='z';x++){
+            for(char y='a'; y<='z';y++){
+                if(x==y) continue;
+                else if(m[x] && m[y])ans= max(ans, f(s, x,y));
+            }
+        }
+        
+        return ans;
+    }
+};
