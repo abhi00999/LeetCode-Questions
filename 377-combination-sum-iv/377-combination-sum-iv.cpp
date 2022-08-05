729ac93f4b2febeff1951530591d27b2a399e782
Abhijeet Kumar Singh <43958202+abhi00999@users.noreply.github.com>
2022-08-05 14:52:45 +0530

    Time: 10 ms (8.26%), Space: 6.9 MB (13.45%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    map<pair<vector<int>, int>, int> m;
+    map<int, int> m;
     
     int f(vector<int> &v, int t){
         if(t==0) return 1;
         if(t<0) return 0;
         
-        if(m.count({v,t})) return m[{v,t}];
+        if(m.count(t)) return m[t];
         
         int res=0;
         for(int i=0;i<v.size();i++){
             res+= f(v, t-v[i]);
         }
-        return m[{v,t}]=res;
+        return m[t]=res;
         
         
     }
